bc914e63b834b52d3e70355c6a84e106f9538fb2
zhangsen <sen.zhang@mobilemd.cn>
2021-01-20 21:58:40 +0800

    栈（有效括号）

--- 20.有效的括号.js
+++ 20.有效的括号.js
@@ -10,7 +10,22 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-
+    var stack = [];
+    var map = {
+        ']': '[',
+        ')': '(',
+        '}': '{'
+    }
+    for (var i = 0; i < s.length; i++) {
+        if (map[s[i]]) {
+            if (stack.pop() !== map[s[i]]) {
+                return false;
+            }
+        } else {
+            stack.push(s[i])
+        }
+    }
+    return !stack.length;
 };
 // @lc code=end
 
