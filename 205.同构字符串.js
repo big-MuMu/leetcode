5c2a0fa60a89e0655765616a28e613f3400c477f
zhangsen <sen.zhang@mobilemd.cn>
2021-02-07 22:02:34 +0800

    同构

--- 205.同构字符串.js
+++ 205.同构字符串.js
@@ -11,19 +11,37 @@
  * @return {boolean}
  */
 var isIsomorphic = function(s, t) {
-    var hash = {};
-    var res = [];
+    // var hash = {};
+    // var res = [];
+    // for (var i = 0; i < s.length; i++) {
+    //     if (hash[s[i]]) {
+    //         if (hash[s[i]] !== t[i]) {
+    //             return false;
+    //         }
+    //     } else {
+    //         if (res.includes(t[i])) {
+    //             return false;
+    //         }
+    //         res.push(t[i]);
+    //         hash[s[i]] = t[i];
+    //     }
+    // }
+    // return true;
+
+    var hash1 = {};
+    var hash2 = {};
+
     for (var i = 0; i < s.length; i++) {
-        if (hash[s[i]]) {
-            if (hash[s[i]] !== t[i]) {
-                return false;
-            }
-        } else {
-            if (res.includes(t[i])) {
-                return false;
-            }
-            res.push(t[i]);
-            hash[s[i]] = t[i];
+        if (!hash1[s[i]]) {
+            hash1[s[i]] = t[i]
+        }
+
+        if (!hash2[t[i]]) {
+            hash2[t[i]] = s[i]
+        }
+
+        if (hash1[s[i]] !== t[i] || hash2[t[i]] !== s[i]) {
+            return false;
         }
     }
     return true;
